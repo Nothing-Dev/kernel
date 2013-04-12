7a2109ab34f5ab7e34fa6f0161d92cce37bbb5ef
Naseer Ahmed <naseer@codeaurora.org>
2013-04-11 20:06:06 -0400

    msm: display: plane alpha support
    Clean up the blending rule to follow blend_op set from hwc.
    Use modulate alpha for plane alpha when it is not 0xff.
    Keep backward compatibility if blend_op is not set.
    
    Change-Id: I02fd3c3c7b5ace2e6eec22b2db0284161404a0fc
    Signed-off-by: Ken Zhang <kenz@codeaurora.org>
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>
    Signed-off-by: Nothing-Dev <alfredomartinez_1423@hotmail.com>
    
    Conflicts:
    	drivers/video/msm/mdp4_overlay.c
    	include/linux/msm_mdp.h

--- include/linux/msm_mdp.h
+++ include/linux/msm_mdp.h
@@ -308,6 +308,28 @@ struct msmfb_writeback_data {
 	struct msmfb_img img;
 };
 
+<<<<<<< HEAD
+=======
+struct dpp_ctrl {
+	/*
+	 *'sharp_strength' has inputs = -128 <-> 127
+	 *  Increasingly positive values correlate with increasingly sharper
+	 *  picture. Increasingly negative values correlate with increasingly
+	 *  smoothed picture.
+	 */
+	int8_t sharp_strength;
+	int8_t hsic_params[NUM_HSIC_PARAM];
+};
+
+enum {
+	BLEND_OP_NOT_DEFINED = 0,
+	BLEND_OP_OPAQUE,
+	BLEND_OP_PREMULTIPLIED,
+	BLEND_OP_COVERAGE,
+	BLEND_OP_MAX,
+};
+
+>>>>>>> b9f82be... msm: display: plane alpha support
 struct mdp_overlay {
 	struct msmfb_img src;
 	struct mdp_rect src_rect;
@@ -316,6 +338,7 @@ struct mdp_overlay {
 	uint32_t is_fg;		/* control alpha & transp */
 	uint32_t alpha;
 	uint32_t transp_mask;
+	uint32_t blend_op;
 	uint32_t flags;
 	uint32_t id;
 	uint32_t user_data[8];
