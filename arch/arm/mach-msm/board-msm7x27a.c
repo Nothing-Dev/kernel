c71ffb3f63ef2bfaa062647faf4b24041bfd7e12
Nothing-Dev <alfredomartinez_1423@hotmail.com>
2015-11-29 20:38:38 -0430

    Revert "msm: board7627a: Make CAMERA/MM use CMA heap"
    This reverts commit acba847802953218fa1ca8422d316312e47a1595.

--- arch/arm/mach-msm/board-msm7x27a.c
+++ arch/arm/mach-msm/board-msm7x27a.c
@@ -191,15 +191,8 @@ static struct platform_device ion_dev;
 static int msm_ion_camera_size;
 static int msm_ion_audio_size;
 static int msm_ion_sf_size;
-static int msm_ion_camera_size_carving;
 #endif
 
-#define CAMERA_HEAP_BASE	0x0
-#ifdef CONFIG_CMA
-#define CAMERA_HEAP_TYPE	ION_HEAP_TYPE_DMA
-#else
-#define CAMERA_HEAP_TYPE	ION_HEAP_TYPE_CARVEOUT
-#endif
 
 static struct android_usb_platform_data android_usb_pdata = {
 	.update_pid_and_serial_num = usb_diag_update_pid_and_serial_num,
@@ -801,11 +794,6 @@ static void fix_sizes(void)
 	msm_ion_camera_size = pmem_adsp_size;
 	msm_ion_audio_size = (MSM_PMEM_AUDIO_SIZE + PMEM_KERNEL_EBI1_SIZE);
 	msm_ion_sf_size = pmem_mdp_size;
-#ifdef CONFIG_CMA
-	msm_ion_camera_size_carving = 0;
-#else
-	msm_ion_camera_size_carving = msm_ion_camera_size;
-#endif
 #endif
 }
 
@@ -851,7 +839,7 @@ struct ion_platform_heap msm7x27a_heaps[] = {
 			.name	= ION_CAMERA_HEAP_NAME,
 			.memory_type = ION_EBI_TYPE,
 			.extra_data = (void *)&co_mm_ion_pdata,
-			.priv	= (void *)&ion_cma_device.dev,
+			.priv = (void *)&ion_cma_device.dev,
 		},
 		/* PMEM_AUDIO */
 		{
@@ -974,9 +962,8 @@ static void __init size_ion_devices(void)
 static void __init reserve_ion_memory(void)
 {
 #if defined(CONFIG_ION_MSM) && defined(CONFIG_MSM_MULTIMEDIA_USE_ION)
+	msm7x27a_reserve_table[MEMTYPE_EBI1].size += msm_ion_camera_size;
 	msm7x27a_reserve_table[MEMTYPE_EBI1].size += msm_ion_audio_size;
-	msm7x27a_reserve_table[MEMTYPE_EBI1].size +=
-		msm_ion_camera_size_carving;
 	msm7x27a_reserve_table[MEMTYPE_EBI1].size += msm_ion_sf_size;
 #endif
 }
@@ -1005,12 +992,13 @@ static void __init msm7x27a_reserve(void)
 {
 	reserve_info = &msm7x27a_reserve_info;
 	msm_reserve();
+
 #ifdef CONFIG_CMA
 	dma_declare_contiguous(
 			&ion_cma_device.dev,
 			msm_ion_camera_size,
 			CAMERA_HEAP_BASE,
-			0xa0000000);
+			0x26000000);
 #endif
 
 }
